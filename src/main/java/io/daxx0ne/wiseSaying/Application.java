c7dd290cc532eb45ab3e4894644b40dc68e6ece9
daxx0ne <jcn07253@gmail.com>
2023-03-07 21:05:57 +0900

    명령 입력 및 등록, 종료 구현

--- src/main/java/io/daxx0ne/wiseSaying/Application.java
+++ src/main/java/io/daxx0ne/wiseSaying/Application.java
@@ -1,6 +1,42 @@
 package io.daxx0ne.wiseSaying;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Application {
 
-    //
+    private final Scanner sc;
+
+    //new Application(sc).run();
+
+    public Application(Scanner sc){
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        int number = 1;
+
+        while(true){
+            System.out.print("명령) ");
+            String command = sc.nextLine().trim(); //혹시 모를 공백들을 제거해줌
+
+            if (command.equals("종료")){
+                break;
+
+            } else if (command.equals("등록")){
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
+
+                System.out.printf("%d번 명언이 동록되었습니다.\n", number);
+                number++;
+
+            }
+        }
+
+    }
 }
--- src/main/java/io/daxx0ne/wiseSaying/Main.java
+++ src/main/java/io/daxx0ne/wiseSaying/Main.java
@@ -1,8 +1,16 @@
 package io.daxx0ne.wiseSaying;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Scanner sc = new Scanner(System.in);
+
+        Application application = new Application(sc);
+        application.run();
+
+        sc.close();
     }
 }
